de6664a7deb7a45a3f22984e133ffe512416c4f2
sedhha <activity.schoolsh2@gmail.com>
2023-09-03 19:12:23 +0530

    Lazy initialization of beans

--- src/main/java/com/first/spring/app/firstspringapp/classes/BasketBallCoach.java
+++ src/main/java/com/first/spring/app/firstspringapp/classes/BasketBallCoach.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BasketBallCoach implements Coach {
+
+    public BasketBallCoach() {
+        System.out.println("Basket-Ball Coach initialized");
+    }
     @Override
     public String getDailyWorkout() {
         return "Hey there! Let's make it to hoops";
--- src/main/java/com/first/spring/app/firstspringapp/classes/CricketCoach.java
+++ src/main/java/com/first/spring/app/firstspringapp/classes/CricketCoach.java
@@ -1,10 +1,16 @@
 package com.first.spring.app.firstspringapp.classes;
 
 import com.first.spring.app.firstspringapp.interfaces.Coach;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("Cricket-Ball Coach initialized");
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
